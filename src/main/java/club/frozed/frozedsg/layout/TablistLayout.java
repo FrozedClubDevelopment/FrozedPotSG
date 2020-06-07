cb74460881b7b41608da0fa485091d49f3a2628f
Elb1to <elbitomc@gmail.com>
2020-06-07 16:31:58 -0500

    Made tablist more configurable

--- src/main/java/club/frozed/frozedsg/layout/TablistLayout.java
+++ src/main/java/club/frozed/frozedsg/layout/TablistLayout.java
@@ -44,16 +44,16 @@ public class TablistLayout implements ZigguratAdapter {
         if (gameManager.getGameState() == GameState.LOBBY) {
             objects.add(new BufferedTabObject().slot(21).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.title")));
 
-            objects.add(new BufferedTabObject().slot(3).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.you")));
-            objects.add(new BufferedTabObject().slot(4).text(ChatColor.translateAlternateColorCodes('&', "&fName: &b" + player.getName())));
+            objects.add(new BufferedTabObject().slot(3).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.you"), player)));
+            objects.add(new BufferedTabObject().slot(4).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.yourName"), player)));
 
             objects.add(new BufferedTabObject().slot(23).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.server")));
-            objects.add(new BufferedTabObject().slot(24).text(ChatColor.translateAlternateColorCodes('&', "&fName: &b" + gameManager.getServerName())));
+            objects.add(new BufferedTabObject().slot(24).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.serverName"), player)));
 
             objects.add(new BufferedTabObject().slot(43).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.state")));
-            objects.add(new BufferedTabObject().slot(44).text(ChatColor.translateAlternateColorCodes('&', "&fPlayers: &b" + playerManager.getLobbyPlayers().size())));
-            objects.add(new BufferedTabObject().slot(45).text(ChatColor.translateAlternateColorCodes('&', "&fMax Players: &b" + gameManager.getMaxPlayers())));
-            objects.add(new BufferedTabObject().slot(46).text(ChatColor.translateAlternateColorCodes('&', "&fRequired: &b" + gameManager.getRequiredPlayersToJoin())));
+            objects.add(new BufferedTabObject().slot(44).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.playersAmount"), player)));
+            objects.add(new BufferedTabObject().slot(45).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.maxPlayersAmount"), player)));
+            objects.add(new BufferedTabObject().slot(46).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.requiredPlayers"), player)));
 
             int x = player.getLocation().getBlockX();
             int z = player.getLocation().getBlockZ();
@@ -61,33 +61,27 @@ public class TablistLayout implements ZigguratAdapter {
             objects.add(new BufferedTabObject().slot(49).text(ChatColor.translateAlternateColorCodes('&', "&7(&f" + x + "&7, &f" + z + "&7)")));
 
             objects.add(new BufferedTabObject().slot(51).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.player_info")));
-            objects.add(new BufferedTabObject().slot(52).text(ChatColor.translateAlternateColorCodes('&', "&fKills: &b" + data.getKills().getAmount())));
-            objects.add(new BufferedTabObject().slot(53).text(ChatColor.translateAlternateColorCodes('&', "&fDeaths: &b" + data.getDeaths().getAmount())));
-            objects.add(new BufferedTabObject().slot(54).text(ChatColor.translateAlternateColorCodes('&', "&fKDR: &b" + data.getKdr())));
-            objects.add(new BufferedTabObject().slot(55).text(ChatColor.translateAlternateColorCodes('&', "&fWins: &b" + data.getWins().getAmount())));
+            objects.add(new BufferedTabObject().slot(52).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.killsAmount"), player)));
+            objects.add(new BufferedTabObject().slot(53).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.deathsAmount"), player)));
+            objects.add(new BufferedTabObject().slot(54).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.KDRAmount"), player)));
+            objects.add(new BufferedTabObject().slot(55).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.winsAmount"), player)));
 
             objects.add(new BufferedTabObject().slot(20).text(PotSG.getInstance().getConfiguration("tablist").getString("tablist.bottom_left")));
             objects.add(new BufferedTabObject().slot(40).text(PotSG.getInstance().getConfiguration("tablist").getString("tablist.bottom_center")));
             objects.add(new BufferedTabObject().slot(60).text(PotSG.getInstance().getConfiguration("tablist").getString("tablist.bottom_right")));
-
-            /* Top 5 Leaderboard
-            objects.add(new BufferedTabObject().slot(8).text(PotSG.getInstance().getConfiguration("tablist").getString("isInLobby.top5wins")));
-            for (top5Slot = 9; top5Slot < 14; top5Slot++) {
-                objects.add(new BufferedTabObject().slot(top5Slot).text(getTop5Wins()));
-            }*/
         }
 
         if (gameManager.getGameState() == GameState.PREMATCH || gameManager.getGameState() == GameState.INGAME || gameManager.getGameState() == GameState.ENDING) {
             objects.add(new BufferedTabObject().slot(21).text(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.title")));
 
             objects.add(new BufferedTabObject().slot(3).text(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.players_title")));
-            objects.add(new BufferedTabObject().slot(4).text(ChatColor.translateAlternateColorCodes('&', "&b" + playerManager.getGamePlayers().size() + "&f players")));
+            objects.add(new BufferedTabObject().slot(4).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.ingameAmount"), player)));
 
             objects.add(new BufferedTabObject().slot(23).text(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.server")));
-            objects.add(new BufferedTabObject().slot(24).text(ChatColor.translateAlternateColorCodes('&', "&fName: &b" + gameManager.getServerName())));
+            objects.add(new BufferedTabObject().slot(24).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.serverName"), player)));
 
             objects.add(new BufferedTabObject().slot(43).text(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.spectators_title")));
-            objects.add(new BufferedTabObject().slot(44).text(ChatColor.translateAlternateColorCodes('&', "&b" + playerManager.getSpectatorPlayers().size() + "&f watching")));
+            objects.add(new BufferedTabObject().slot(44).text(replace(PotSG.getInstance().getConfiguration("tablist").getString("isInMatch.spectatorsAmount"), player)));
 
             int playerSlot = 6;
             for (Player online : Bukkit.getServer().getOnlinePlayers()) {
@@ -111,20 +105,27 @@ public class TablistLayout implements ZigguratAdapter {
         return objects;
     }
 
-    /*public String getTop5Wins() {
-        int pos = 1;
-        List<Document> top5Wins = MongoManager.getInstance().getStatsCollection().find().limit(5).sort(new BasicDBObject("wins", -1)).into(new ArrayList<>());
+    public String replace(String string, Player player) {
+        PlayerData data = PlayerDataManager.getInstance().getByUUID(player.getUniqueId());
+        GameManager gameManager = GameManager.getInstance();
+        PlayerManager playerManager = PlayerManager.getInstance();
 
-        for (Document document : top5Wins) {
-            String formattedTop5 = Color.translate(
-                    "&f"
-                    + pos
-                    + ". &b"
-                    + document.getString("name") == null ? "null" : document.getString("name")
-                    + " &8(" + document.get("wins").toString() + "&8)");
-            pos++;
-        }
-        return getTop5Wins();
-    }*/
+        return string
+                .replaceAll("<playerName>", player.getName())
+                .replaceAll("<server_name>", gameManager.getServerName())
+                .replaceAll("<max_players>", String.valueOf(gameManager.getMaxPlayers()))
+                .replaceAll("<require_players>", String.valueOf(gameManager.getRequiredPlayersToJoin()))
+                .replaceAll("<countdown_players>", String.valueOf(playerManager.getLobbyPlayers().size()))
+                .replaceAll("<prematch_players_size>", String.valueOf(playerManager.getPrematchPlayers().size()))
+
+                .replaceAll("<game_players>", String.valueOf(playerManager.getGamePlayers().size()))
+                .replaceAll("<spectators_size>", String.valueOf(playerManager.getSpectatorPlayers().size()))
+
+                .replaceAll("<playerKills>", String.valueOf(data.getKills().getAmount()))
+                .replaceAll("<playerDeaths>", String.valueOf(data.getDeaths().getAmount()))
+                .replaceAll("<playerKDR>", String.valueOf(data.getKdr()))
+                .replaceAll("<playerWins>", String.valueOf(data.getWins().getAmount()))
+                ;
+    }
 
 }
