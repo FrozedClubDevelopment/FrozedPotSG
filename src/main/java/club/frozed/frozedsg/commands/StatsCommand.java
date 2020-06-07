5bf64182810bdcf2eb92a96e2cf0cb3476f2c12c
Elb1to <elbitomc@gmail.com>
2020-06-07 16:32:14 -0500

    Deleted old stats output

--- src/main/java/club/frozed/frozedsg/commands/StatsCommand.java
+++ src/main/java/club/frozed/frozedsg/commands/StatsCommand.java
@@ -11,7 +11,6 @@ import club.frozed.frozedsg.utils.command.CommandArgs;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-
 public class StatsCommand extends BaseCommand {
 
     @Command(name = "stats")
@@ -35,18 +34,6 @@ public class StatsCommand extends BaseCommand {
             player.sendMessage(Color.translate(GameManager.getInstance().getGamePrefix() + "&cData of that player is not loaded."));
             return;
         }
-
-        /*player.sendMessage(Color.translate("&7&m-----------------------"));
-        player.sendMessage(Color.translate("&e&l" + target.getName() + "'s Statistics"));
-        player.sendMessage(Color.translate("&7* &eKills&7: &6" + targetData.getKills().getAmount()));
-        player.sendMessage(Color.translate("&7* &eDeaths&7: &6" + targetData.getDeaths().getAmount()));
-        player.sendMessage(Color.translate("&7* &eWins&7: &6" + targetData.getWins().getAmount()));
-        player.sendMessage(Color.translate("&7* &ePoints&7: &6" + targetData.getPoints().getAmount()));
-        player.sendMessage(Color.translate("&7* &eGames Played&7: &6" + targetData.getGamesPlayed().getAmount()));
-        player.sendMessage(Color.translate("&7* &eHighest KillStreak&7: &6" + targetData.getKillStreak().getAmount()));
-        player.sendMessage(Color.translate("&7* &eKDR&7: &6" + targetData.getKdr()));
-        player.sendMessage(Color.translate("&7&m-----------------------")); */
         player.openInventory(InventoryManager.getInstance().getStatsInventory(targetData));
-
     }
 }
